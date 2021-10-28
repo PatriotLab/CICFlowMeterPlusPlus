20b65ac472136b75e01ece49e06704b255668a87
wes1095 <dylan.westlund@trojans.dsu.edu>
2021-10-28 09:25:05 -0700

    Update winBytes feature

--- src/main/java/cic/cs/unb/ca/jnetpcap/features/winBytes.java
+++ src/main/java/cic/cs/unb/ca/jnetpcap/features/winBytes.java
@@ -1,4 +1,33 @@
 package cic.cs.unb.ca.jnetpcap.features;
 
-public class winBytes {
+import cic.cs.unb.ca.jnetpcap.BasicPacketInfo;
+
+public class winBytes extends FeatureCollection{
+    private int fwd_win_bytes = 0;
+    private int bwd_win_bytes = 0;
+
+    public winBytes() {
+        fields = new FeatureCollection.FieldBuilder()
+                .addField(() -> fwd_win_bytes, "FWD Init Win Bytes")
+                .addField(() -> bwd_win_bytes, "Bwd Init Win Bytes")
+                .build();
+    }
+
+    @Override
+    public void onPacket(BasicPacketInfo packet) {
+        boolean isBackward = packet.isBwdPacket;
+        int TCPWindow = packet.getTCPWindow();
+
+        if(isBackward){
+            if(bwd_win_bytes == 0){
+                bwd_win_bytes = TCPWindow;
+            }
+        }
+        else{
+            if(fwd_win_bytes == 0){
+                fwd_win_bytes = TCPWindow;
+            }
+        }
+
+    }
 }
