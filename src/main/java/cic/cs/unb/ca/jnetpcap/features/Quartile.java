6e3b5cbc550d32d18cc312bf779da8c3968351e5
wes1095 <dylan.westlund@trojans.dsu.edu>
2022-02-01 13:13:46 -0800

    Finish Quartile class

--- src/main/java/cic/cs/unb/ca/jnetpcap/features/Quartile.java
+++ src/main/java/cic/cs/unb/ca/jnetpcap/features/Quartile.java
@@ -24,40 +24,79 @@ public class Quartile extends FeatureCollection{
         values.add(value);
         Collections.sort(values);
         size = values.size();
-        Q2 = getQuartile(0, size, values);
-        Q1 = getQuartile(0, Math.round(size / 2), values);
-        Q3 = getQuartile(Math.round(size / 2) + 1, size, values);
+        Q2 = getSecond();
+        Q1 = getFirst();
+        Q3 = getThird();
 
     }
 
-    private float getQuartile(int low, int high, ArrayList<Float> values){
-        if(high == 0){
-            return 0;
+    private float getFirst(){
+        if(size < 3){
+            return -1;
         }
-        if(high == 1){
-            return values.get(0);
-
-        }
-        if(high == 2){
-            return (values.get(0) + values.get(1))/2;
-        }
-        float quartile = 0;
-        int range = high - low;
-        float dividend = 0.5f;
-        if (low != 0){
-            dividend = 0.75f;
-        }
-        if (range % 2 == 0){
-            // even
-            float term1 = values.get(Math.round((high*dividend)) - 1);
-            float term2 = values.get(Math.round(high*dividend));
-            quartile = (term1 + term2) / 2;
+        int midpoint = (size / 2);
+        if(size % 2 != 0){
+            // odd size
+            if (midpoint % 2 == 0) {
+                // even quarter
+                return (values.get(midpoint / 2) + values.get((midpoint / 2) - 1)) / 2;
+            } else {
+                // odd quarter
+                return (values.get(midpoint / 2));
+            }
         }
         else{
-            // odd
-            quartile = values.get(Math.round(((high + 1)*dividend)) - 1);
+            //even size
+            if(midpoint % 2 == 0){
+                // even quarter
+                return (values.get(((midpoint-1)/2)+1) + values.get(midpoint))/2;
+            }
+            else{
+                // odd quarter
+                return (values.get((midpoint-1)/2));
+            }
         }
 
-        return quartile;
+    }
+
+    private float getSecond(){
+        if(size % 2 != 0){
+            // odd size, pick middle
+            return values.get(size/2);
+        }
+        else{
+            // even size, get average of two middle
+            return (values.get(size/2) + values.get((size/2)-1))/2;
+        }
+    }
+
+    private float getThird(){
+        if(size < 3){
+            return -1;
+        }
+        int midpoint = size/2;
+        if(size % 2 != 0){
+            // odd size
+            if(midpoint % 2 != 0){
+                // odd quarter
+                return (values.get((int) (size * 0.75d)));
+            }
+            else{
+                // even quarter
+                return (values.get((int) (size*.75d)) + values.get((int) ((size*.75d)+1)))/2;
+            }
+        }
+        else{
+            // even size
+            if(midpoint % 2 != 0){
+                // odd quarter
+                return values.get((int) (size*.75d));
+            }
+            else{
+                // even quarter
+                return (values.get((int) (size*.75d)) + values.get((int) ((size*.75d)-1)))/2;
+            }
+        }
+
     }
 }
