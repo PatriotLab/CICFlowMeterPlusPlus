b04848480765c57654f6ecf113d111747b761b5a
Julia Scheaffer <me@gisch.dev>
2022-05-31 11:46:43 -0500

    TDigest working

--- src/main/java/cic/cs/unb/ca/jnetpcap/features/TimeToLive.java
+++ src/main/java/cic/cs/unb/ca/jnetpcap/features/TimeToLive.java
@@ -4,18 +4,15 @@ import cic.cs.unb.ca.jnetpcap.BasicPacketInfo;
 
 public class TimeToLive extends FeatureCollection {
     private final StatsFeature packetTTLstats = new StatsFeature();
-    private final Quartile packetTTLquartiles = new Quartile();
 
     public TimeToLive() {
         new FeatureCollection.FieldBuilder()
                 .addField(packetTTLstats, "TTL {0}")
-                .addField(packetTTLquartiles, "TTL {0}")
                 .build(this);
     }
 
     @Override
     public void onPacket(BasicPacketInfo packet) {
         packetTTLstats.addValue(packet.ttl);
-        packetTTLquartiles.add(packet.ttl);
     }
 }
