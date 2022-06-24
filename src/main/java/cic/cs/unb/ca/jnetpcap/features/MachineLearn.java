8acbd2987bb9d96142fc7df4243ee9f4c84d6edc
Michael Fahnlander <michael.fahnlander@trojans.dsu.edu>
2022-06-24 08:30:22 -0700

    test ML

---
+++ src/main/java/cic/cs/unb/ca/jnetpcap/features/MachineLearn.java
@@ -0,0 +1,39 @@
+package cic.cs.unb.ca.jnetpcap.features;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class MachineLearn extends FeatureCollection{
+    private String label;
+    private double accuracy;
+    public MachineLearn(){
+        new FeatureCollection.FieldBuilder()
+                .addField(() -> label, "Label")
+                .addField(() -> accuracy, "Accuracy")
+                .build(this);
+    }
+
+    //Install Machine Learning Python Packages and dependencies
+    try {
+        Process process = Runtime.getRuntime().exec("pip install -U scikit-learn && pip install sklearn-pandas && pip install sklearn2pmml");
+        BufferedReader input = new BufferedReader(new InputStreamReader(process.getInputStream()));
+        String output = null;
+
+        while((output = input.readLine()) != null) {
+            System.out.println(output);
+        }
+    } catch(Exception e) {
+        //assert false;//intelij said the next line would throw null pointer '\_/(O_O)\_/'
+        System.out.println(e.toString());
+        e.printStackTrace();
+    }
+    //Get Finished CSV
+    
+
+    //Make Label Column and Accuracy column?
+
+
+    //Run model on CSV
+
+
+}
