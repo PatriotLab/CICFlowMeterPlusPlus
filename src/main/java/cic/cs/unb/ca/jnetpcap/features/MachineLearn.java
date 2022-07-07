41777fb513364d04a0860f1f1e030780e4fe9d7c
Michael Fahnlander <michael.fahnlander@trojans.dsu.edu>
2022-07-07 11:17:12 -0500

    add model, proper code

--- src/main/java/cic/cs/unb/ca/jnetpcap/features/MachineLearn.java
+++ src/main/java/cic/cs/unb/ca/jnetpcap/features/MachineLearn.java
@@ -1,9 +1,13 @@
 package cic.cs.unb.ca.jnetpcap.features;
 
 import cic.cs.unb.ca.jnetpcap.BasicPacketInfo;
+import org.jpmml.evaluator.*;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.InputStreamReader;
+import java.util.List;
+import java.util.Map;
 
 public class MachineLearn extends FeatureCollection{
     private String label;
@@ -17,26 +21,55 @@ public class MachineLearn extends FeatureCollection{
 
     @Override
     public void onPacket(BasicPacketInfo packet) {
-        //Install Machine Learning Python Packages and dependencies
-        try {
-            Process process = Runtime.getRuntime().exec("pip install -U scikit-learn && pip install sklearn-pandas && pip install sklearn2pmml");
-            BufferedReader input = new BufferedReader(new InputStreamReader(process.getInputStream()));
-            String output = null;
+        // Building a model evaluator from a PMML file
+            Evaluator evaluator = new LoadingModelEvaluatorBuilder()
+                    .load(new File("model.pmml"))//change this
+                    .build();
 
-            while ((output = input.readLine()) != null) {
-                System.out.println(output);
+        // Perforing the self-check
+            evaluator.verify();
+
+        // Printing input (x1, x2, .., xn) fields
+            List<InputField> inputFields = evaluator.getInputFields();
+            System.out.println("Input fields: " + inputFields);
+
+        // Printing primary result (y) field(s)
+            List<TargetField> targetFields = evaluator.getTargetFields();
+            System.out.println("Target field(s): " + targetFields);
+
+        // Printing secondary result (eg. probability(y), decision(y)) fields
+            List<OutputField> outputFields = evaluator.getOutputFields();
+            System.out.println("Output fields: " + outputFields);
+
+        // Iterating through columnar data (eg. a CSV file, an SQL result set)
+            while(true){
+                // Reading a record from the data source
+                //is there an existing record that we can point the machine learning at to ingest data?
+                Map<String, ?> arguments = 0; //Set arguments equal to a record
+                if(arguments == null){
+                    break;
+                }
+
+                // Evaluating the model
+                Map<String, ?> results = evaluator.evaluate(arguments);
+
+                // Decoupling results from the JPMML-Evaluator runtime environment
+                results = EvaluatorUtil.decodeAll(results);
+
+                // Writing a record to the data sink
+                writeRecord(results);
             }
-        } catch (Exception e) {
-            //assert false;//intelij said the next line would throw null pointer '\_/(O_O)\_/'
-            System.out.println(e.toString());
-            e.printStackTrace();
-        }
-        //Get Finished CSV
 
-        //Make Label Column and Accuracy column?
+        // Making the model evaluator eligible for garbage collection
+        evaluator = null;
+    }
 
+    private Map<String,?> readRecord() {
+        //This might be redundant if we can give the model a record of all the pcap data
 
-        //Run model on CSV
-
+        return;
+    }
+    private void writeRecord(Map<String,?> results) {
+        //set label and accuracy values here?
     }
 }
